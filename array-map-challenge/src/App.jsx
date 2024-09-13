1529fe6e2c72bd144ebdc04a5c88dead75dbf0de
ojha <ojhaatish11@gmail.com>
2024-09-14 01:03:02 +0545

    Project 2 : learn about array.map and done some challenges

---
+++ array-map-challenge/src/App.jsx
@@ -0,0 +1,40 @@
+import Jokes from "./components/Jokes"
+import JokeData from "./components/JokeData"
+
+
+export default function App(){
+  
+  const jokeElement = JokeData.map((joke) => {
+    return <Jokes
+              setup={joke.setup}
+              punchline={joke.punchline}
+              upvotes={joke.upVotes}
+              downvotes={joke.downVotes}
+            />
+  })
+  return(
+    <div>
+      {jokeElement}
+    </div>
+  )
+}
+
+
+
+
+// 1. What does the `.map()` array method do?
+// Returns a new array. Whatever gets returned from the callback
+// function provided is placed at the same index in the new array.
+// Usually we take the items from the original array and modify them
+// in some way.
+
+
+// 2. What do we usually use `.map()` for in React?
+// Convert an array of raw data into an array of JSX elements
+// that can be displayed on the page.
+
+
+// 3. Why is using `.map()` better than just creating the components
+//    manually by typing them out?
+// It makes our code more "self-sustaining" - not requiring
+// additional changes whenever the data changes.
